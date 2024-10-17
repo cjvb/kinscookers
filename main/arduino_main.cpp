c03bdabe57d2aa0b892660879cbc5af369a09ff8
Krish Singh <singhkrish4u@gmail.com>
2024-10-17 18:19:20 -0500

    ColorSensorProto

--- main/arduino_main.cpp
+++ main/arduino_main.cpp
@@ -26,6 +26,19 @@ limitations under the License.
 #include <ESP32SharpIR.h>
 #include <QTRSensors.h>
 
+#include <Wire.h>
+#include <Arduino_APDS9960.h>
+#include <bits/stdc++.h>
+#include <RGBConverter.h>
+
+#define APDS9960_INT 2
+#define I2C_SDA 21
+#define I2C_SCL 22
+#define I2C_FREQ 100000
+
+TwoWire I2C_0 = TwoWire(0);
+APDS9960 apds = APDS9960(I2C_0, APDS9960_INT);
+
 GamepadPtr myGamepads[BP32_MAX_GAMEPADS];
 
 // This callback gets called any time a new gamepad is connected.
@@ -41,6 +54,7 @@ void onConnectedGamepad(GamepadPtr gp) {
 }
 
 void onDisconnectedGamepad(GamepadPtr gp) {
+    
     bool foundGamepad = false;
     for (int i = 0; i < BP32_MAX_GAMEPADS; i++) {
         if (myGamepads[i] == gp) {
@@ -51,9 +65,20 @@ void onDisconnectedGamepad(GamepadPtr gp) {
     }
 }
 
+
+
 // Arduino setup function. Runs in CPU 1
 void setup() {
     // Setup the Bluepad32 callbacks
+
+    //sets up I2C protocol
+    I2C_0.begin(I2C_SDA, I2C_SCL, I2C_FREQ);
+
+    //sets up color sensor
+    apds.setInterruptPin(APDS9960_INT);
+    apds.begin();
+    Serial.begin(115200);
+
     BP32.setup(&onConnectedGamepad, &onDisconnectedGamepad);
     BP32.forgetBluetoothKeys();
 
@@ -67,17 +92,61 @@ void setup() {
 
 // Arduino loop function. Runs in CPU 1
 void loop() {
-    BP32.update();
+    
 
-    for (int i = 0; i < BP32_MAX_GAMEPADS; i++) {
-        GamepadPtr myGamepad = myGamepads[i];
-        if (myGamepad && myGamepad->isConnected()) {
-            // TODO: Write your controller code here
+    int r, g, b, a;
+    // Wait until color is read from the sensor 
+    while (!apds.colorAvailable()) { delay(5); }
+    apds.readColor(r, g, b, a);
+    // Read color from sensor apds.readColor(r, g, b, a);
 
-        }
-    }
+    int sum = r + g + b;
+    r /= sum; b /= sum; g /= sum;
 
-    // TODO: Write your periodic code here
+    r *= 256; b *= 256; g *= 256; //convert to RGB relative values
+
+    // import RGB to HSV library 
+
+    double hue, saturation, value;
+    RGBConverterLib::RgbToHsv(static_cast<uint8_t>(r), static_cast<uint8_t>(g), static_cast<uint8_t>(b), hue, saturation, value);
+
+    string Color = classifyToColor(hue);
+    Serial.println(Color);
 
     vTaskDelay(1);
+}
+
+string classifyToColor(int hue) {
+    if (hue < 15)
+  {
+   return "Red";
+  }
+  else if (hue < 45)
+  {
+    return "Orange";
+  }
+  else if (hue < 90)
+  {
+    return "Yellow";
+  }
+  else if (hue < 150)
+  {
+    return "Green";
+  }
+  else if (hue < 210)
+  {
+    return "Cyan";
+  }
+  else if (hue < 270)
+  {
+    return "Blue";
+  }
+  else if (hue < 330)
+  {
+    return "Magenta";
+  }
+  else
+  {
+    return "None";
+  }
 }
